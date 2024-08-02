0a2fec7480b6101720a4a17bd2ce8e6e13debbbe
Berk Erdoğan <erdgnberk@gmail.com>
2024-08-02 12:57:52 +0300

    Project start
    uploading required files

---
+++ src/main/java/com/berkerdgn/Todo_Springboot_BootcampFinalProject/TodoSpringbootBootcampFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.berkerdgn.Todo_Springboot_BootcampFinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSpringbootBootcampFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSpringbootBootcampFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berkerdgn/Todo_Springboot_BootcampFinalProject/TodoSpringbootBootcampFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkerdgn.Todo_Springboot_BootcampFinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringbootBootcampFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
