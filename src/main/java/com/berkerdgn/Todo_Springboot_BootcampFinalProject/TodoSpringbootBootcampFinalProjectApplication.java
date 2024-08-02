180a885f754975723569758b192d432060d6e3a7
Berk Erdoğan <erdgnberk@gmail.com>
2024-08-02 14:52:22 +0300

    Exception Add
    Exception was added

--- src/main/java/com/berkerdgn/Todo_Springboot_BootcampFinalProject/TodoSpringbootBootcampFinalProjectApplication.java
+++ src/main/java/com/berkerdgn/Todo_Springboot_BootcampFinalProject/TodoSpringbootBootcampFinalProjectApplication.java
@@ -1,13 +1,70 @@
 package com.berkerdgn.Todo_Springboot_BootcampFinalProject;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+import java.util.TimeZone;
+
+// Mongo aktif etmek ici
+// @EnableMongoRepositories
+
+// Aspect aktif etmek icin
+// @EnableAspectJAutoProxy(proxyTargetClass = true)
+
+// Asenkron açmak icin
+// @EnableAsync
+
+// SCAN
+//@EntityScan(basePackages = "com.hamitmizrak.seniorfullstack1.data.entity") //Entity bulamadığı zaman
+//@EnableJpaRepositories(basePackages = "com.hamitmizrak.seniorfullstack1.data.repository") //Repository bulamadığı zaman
+//@ComponentScan("com")
+
+// Spring Cache aktif etmek gerekiyor.
+// @EnableCaching
+
+// Auditing Aktif etmek
+//@EnableJpaAuditing(auditorAwareRef = "auditorAwareBeanMethod")
+
+// Spring Security: Şimdilik dahil etme ancak Spring security için gerekli kütüphaneleri dahil
+@SpringBootApplication(exclude = {
+		// SecurityAutoConfiguration.class,
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+// @SpringBootApplication
+// public class FullStackDeveloper12Application {
+
+// Apache Tomcat için: extends SpringBootServletInitializer
+
+// @SpringBootApplication
 public class TodoSpringbootBootcampFinalProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoSpringbootBootcampFinalProjectApplication.class, args);
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
 	}
 
-}
+	public static void main(String[] args) {
+
+		// devtools active isActive
+		// System.setProperty("spring.devtools.restart.enabled","true");
+
+		// PORT Ayarlamak
+		/*
+		 * SpringApplication app = new
+		 * SpringApplication(TurgutUniversitySpringAllInOneApplication.class);
+		 * app.setDefaultProperties(Collections
+		 * .singletonMap("server.port", "8083"));
+		 * app.run(args);
+		 */
+
+		// JOptional pane aktif etmek
+		System.setProperty("java.awt.headless", "false");
+
+		// Main
+		SpringApplication.run(TodoSpringbootBootcampFinalProjectApplication.class, args);
+	} // end PSVM
+
+} // end class
---
+++ src/main/java/com/berkerdgn/Todo_Springboot_BootcampFinalProject/exception/BerkErdgnException.java
@@ -0,0 +1,10 @@
+package com.berkerdgn.Todo_Springboot_BootcampFinalProject.exception;
+
+public class BerkErdgnException extends RuntimeException {
+
+    // Constructor (Parametreli)
+    public BerkErdgnException(String message) {
+        super(message);
+    } // end constructor
+
+}// end class
